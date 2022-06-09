caec48c552ade7f8e5ec513bb2c1594dd77527a1
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-06-09 15:46:07 +0200

    Some small fixes to login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,6 +4,7 @@ import { useAuth } from 'contexts/AuthProvider';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Input } from 'components/Input/Input';
 import { BsTwitter, BsFillLightningChargeFill } from 'react-icons/bs';
+import { MdArticle, MdReviews, MdCreate } from 'react-icons/md';
 import Logo from 'assets/img/articles.svg';
 
 const Login = () => {
@@ -41,17 +42,14 @@ const Login = () => {
   return (
     <div className="h-screen flex">
       <div className="flex flex-col justify-center items-center w-full md:w-2/4 p-6 m-auto h-full">
-        <div className=" flex flex-col items-center justify-between w-full md:w-4/5 max-w-md h-4/5">
-          <div className=" mb-6 self-start justify-start ">
-            <BsFillLightningChargeFill
-              size="3rem"
-              className="text-indigo-500"
-            />
-          </div>
-          <div className="flex flex-col justify-start w-full">
+        <div className=" mb-6  justify-start w-4/5 ">
+          <BsFillLightningChargeFill size="3rem" className="text-indigo-500" />
+        </div>
+        <div className=" flex flex-col items-center justify-center w-full md:w-4/5 max-w-md h-4/5">
+          <div className="flex flex-col justify-start w-full mb-6">
             <h1 className="text-3xl font-semibold self-start mt-6">Login</h1>
             <p className="text-gray-400 self-start mb-6">
-              See your growth and get consulting support!
+              Get access to all articles and interact!
             </p>
           </div>
 
@@ -106,13 +104,23 @@ const Login = () => {
         </div>
       </div>
       <div className="w-2/4 bg-indigo-500 md:flex justify-center items-center hidden">
-        <div className=" flex flex-col items-center justify-around w-4/5 h-4/5">
+        <div className=" flex relative flex-col items-center justify-around w-4/5 h-4/5">
           <img src={Logo} className="w-2/4" />
           <div>
             <h1 className="text-4xl text-gray-50 text-center mb-2">
               Read, review and create articles about things that interests you!
             </h1>
             <p className="text-gray-200 text-center">Everything for free!</p>
+          </div>
+          <span className="w-20 h-20 rounded-full bottom-3/5 right-12 absolute bg-gray-100 flex items-center justify-center">
+            <MdArticle size="3rem" className="text-indigo-600" />
+          </span>
+          <span className="w-20 h-20 rounded-full top-1/3 left-0 absolute bg-gray-100 flex items-center justify-center">
+            <MdReviews size="3rem" className="text-indigo-600" />
+          </span>
+          <div className="w-20 h-20  top-0 right-0 absolute flex items-center justify-center">
+            <span className="w-20 h-20 bg-gray-100  absolute rounded-full"></span>
+            <MdCreate size="3rem" className="text-indigo-600 absolute" />
           </div>
         </div>
       </div>
