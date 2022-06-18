6eb472432c9893bf292c4bbf4657379c36640bed
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-06-18 22:38:30 +0200

    Added article image to article list on homepage

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { useArticles } from 'hooks/useArticles';
 import { Link } from 'react-router-dom';
+import { getArticleImageUrl } from 'utils/article';
 
 const ArticleList = () => {
   const { data: articles } = useArticles();
@@ -11,19 +12,27 @@ const ArticleList = () => {
       <section>
         {articles &&
           articles.map(
-            ({ id, title, body, created_at, user: { fullName } }) => {
+            ({ id, title, body, created_at, imageUrl, user: { fullName } }) => {
               const formatedDate = dayjs(created_at).format('DD.MM.YYYY HH:mm');
               return (
                 <Link to={`/articles/${id}`} key={id}>
-                  <article className="border-2 rounded-md p-4 mb-4 group shadow-md hover:shadow-indigo-400 ease-in-out duration-300">
-                    <h2 className="text-2xl mb-2 group-hover:text-indigo-500">
-                      {title}
-                    </h2>
-                    <p className="line-clamp-3 mb-2">{body}</p>
-                    <p className="text-right">
-                      <span className="font-semibold">{fullName}</span>{' '}
-                      <span className="text-gray-600">{formatedDate}</span>
-                    </p>
+                  <article className="border-2 rounded-md p-4 mb-4 group shadow-md hover:shadow-indigo-400 ease-in-out duration-300 flex gap-3 group">
+                    <div className="w-2/5 h-full overflow-hidden rounded-md">
+                      <img
+                        className=" object-cover h-full w-full group-hover:scale-110 duration-300 "
+                        src={getArticleImageUrl(imageUrl)}
+                      />
+                    </div>
+                    <div className="w-3/5">
+                      <h2 className="text-2xl mb-2 group-hover:text-indigo-500">
+                        {title}
+                      </h2>
+                      <p className="line-clamp-3 mb-2">{body}</p>
+                      <p className="text-right">
+                        <span className="font-semibold">{fullName}</span>{' '}
+                        <span className="text-gray-600">{formatedDate}</span>
+                      </p>
+                    </div>
                   </article>
                 </Link>
               );
--- src/templates/GridTemplate.tsx
+++ src/templates/GridTemplate.tsx
@@ -4,7 +4,7 @@ type GridTemplateProps = {
 
 const GridTemplate = ({ children }: GridTemplateProps) => {
   return (
-    <section className="grid grid-cols-[1fr_2fr_1fr] gap-4 max-w-7xl m-auto">
+    <section className="grid grid-cols-[1fr_3fr_1fr] gap-4 max-w-7xl m-auto">
       {children}
     </section>
   );
